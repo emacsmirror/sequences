6147bd8ba2a6e03d66066d508f64ee3f918e2e7a
Tim Visher <tvisher@monetate.com>
2013-09-08 14:16:16 -0400

    Fixing up header. EOM

--- sequences.el
+++ sequences.el
@@ -1,14 +1,24 @@
 ;;; sequences.el --- Ports of some Clojure sequence functions.
 
-;; This file is not part of GNU Emacs
+;; CC BY Tim Visher
 
-;;; License:
+;; Author: Tim Visher <tim.visher@gmail.com>
+;; Keywords: convenience
+;; Package-Requires: ((emacs "24"))
+;; Version: 0.1.0
 
-;; [CC BY](http://creativecommons.org/licenses/by/3.0/)
+;; This file is not part of GNU Emacs.
 
-;;; Contributors:
+;; This work is licensed under the Creative Commons Attribution 3.0
+;; Unported License. To view a copy of this license, visit
+;; <http://creativecommons.org/licenses/by/3.0/> or send a letter to
+;; Creative Commons, 444 Castro Street, Suite 900, Mountain View,
+;; California, 94041, USA.
 
-;; Tim Visher ([@timvisher](https://twitter.com/timvisher))
+;;; Commentary:
+
+;; Ports of Clojure sequence functions that I find useful to have
+;; around. Maybe others will too?
 
 ;;; Code:
 
